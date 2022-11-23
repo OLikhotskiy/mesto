11f5b980e44b0ebd4e9437a89d2a4b33b3914d66
Oleg Likhotskiy <lihotsky777@gmail.com>
2022-11-23 22:19:48 +0200

    fix: закрытие попапа

--- scripts/index.js
+++ scripts/index.js
@@ -2,11 +2,11 @@ const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close');
 const popupInputName = popupElement.querySelector('.popup__input_type_name');
 const popupInputAbout = popupElement.querySelector('.popup__input_type_about');
-const popupForm = popupElement.querySelector('.popup__form');
+const popupForm = document.querySelector('.popup__form');
 
 const profileElement = document.querySelector('.profile');
-const profileName = profileElement.querySelector('.profile__name');
-const profileAbout = profileElement.querySelector('.profile__about');
+const profileName = document.querySelector('.profile__name');
+const profileAbout = document.querySelector('.profile__about');
 const popupOpenButtonElement = profileElement.querySelector('.profile__edit');
 
 const openPopup = function() {
@@ -23,7 +23,7 @@ function formSubmitHandler (evt) {
     evt.preventDefault(); 
     profileName.textContent = popupInputName.value;
     profileAbout.textContent = popupInputAbout.value;
-    ClosePopup();
+    closePopup();
 }
 
 popupOpenButtonElement.addEventListener('click', openPopup);
