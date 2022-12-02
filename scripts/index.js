dd5ff9386d821d9a1d64e397c60e0a23650a5a39
Oleg Likhotskiy <lihotsky777@gmail.com>
2022-12-02 18:59:28 +0200

    feat: создать лайки. создать делиты

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,37 @@
-const popupElement = document.querySelector('.popup');
-const popupCloseButtonElement = popupElement.querySelector('.popup__close');
-const popupInputName = popupElement.querySelector('.popup__input_type_name');
-const popupInputAbout = popupElement.querySelector('.popup__input_type_about');
+const popupElement = document.querySelectorAll('.popup');
+const popupElementEdit = document.querySelector('.popup__type_edit');
+const popupElementAdd = document.querySelector('.popup__type_add');
+const popupCloseButtonElement = document.querySelectorAll('.popup__close');
+
+const popupInputName = document.querySelector('.popup__input_type_name');
+const popupInputAbout = document.querySelector('.popup__input_type_about');
+const popupInputPicture = document.querySelector('.popup__input_type_picture');
+const popupInputTitle = document.querySelector('.popup__input_type_title');
 const popupForm = document.querySelector('.popup__form');
 
 const profileElement = document.querySelector('.profile');
 const profileName = document.querySelector('.profile__name');
 const profileAbout = document.querySelector('.profile__about');
-const popupOpenButtonElement = profileElement.querySelector('.profile__edit');
+const elementPicture = document.querySelector('.element__picture');
+const elementTitle = document.querySelector('.element__title');
 
-const openPopup = function() {
-    popupElement.classList.add('popup_is-open');
+const popupOpenButtonEditElement = document.querySelector('.profile__edit');
+const popupOpenButtonAddElement = document.querySelector('.profile__add');
+const elementLike = document.querySelectorAll('.element__like');
+
+const openPopupEdit = function() {
+    popupElementEdit.classList.add('popup_is-open');
     popupInputName.value = profileName.textContent;
     popupInputAbout.value = profileAbout.textContent;
 };
 
+const openPopupAdd = function() {
+    popupElementAdd.classList.add('popup_is-open');
+};
+
 const closePopup = function() {
-    popupElement.classList.remove('popup_is-open');
+    popupElementAdd.classList.remove('popup_is-open');
+    popupElementEdit.classList.remove('popup_is-open');
 };
 
 function formSubmitHandler (evt) {
@@ -26,6 +41,41 @@ function formSubmitHandler (evt) {
     closePopup();
 }
 
-popupOpenButtonElement.addEventListener('click', openPopup);
-popupCloseButtonElement.addEventListener('click', closePopup);
+popupOpenButtonEditElement.addEventListener('click', openPopupEdit);
+popupOpenButtonAddElement.addEventListener('click', openPopupAdd);
+popupCloseButtonElement.forEach(button => button.addEventListener('click', closePopup));
 popupForm.addEventListener('submit', formSubmitHandler);
+
+for (let i = 0; i < elementLike.length; i++) {elementLike[i].addEventListener('click', function() {
+  elementLike[i].classList.toggle ('element__like_active');
+  }
+      );
+};
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ]; 
+  
