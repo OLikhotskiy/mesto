e66aeef5b580b891dea65b57b2be0da985eb4326
Oleg Likhotskiy <lihotsky777@gmail.com>
2023-02-18 13:02:20 +0200

    Updates

--- src/utils/constants.js
+++
@@ -1,7 +0,0 @@
-export const popupOpenButtonEditElement = document.querySelector('.profile__edit');
-export const popupElementEdit = document.querySelector('.popup_type_profile-edit');
-export const popupInputName = document.querySelector('.popup__input_type_name');
-export const popupInputAbout = document.querySelector('.popup__input_type_about');
-
-export const popupOpenButtonAddElement = document.querySelector('.profile__add');
-export const popupElementAdd = document.querySelector('.popup_type_add');
